dffc276fe32a467bcc5f9fa9968329923daf9cb4
Tomek Banach <tomek@banciur.org>
2017-08-02 01:07:49 +0200

    Initial version of myStats

--- src/reducers/myStats.js
+++ src/reducers/myStats.js
@@ -1,3 +1,5 @@
+import { SET_USER_ADDRESS, SET_USER_LOADING, LOAD_USER_NEUMARKS } from '../actions/constants';
+
 const initialState = {
   loading: false,
   address: null,
@@ -5,7 +7,23 @@ const initialState = {
 };
 
 export default function (state = initialState, action) {
-  switch (action) {
+  const { type, payload } = action;
+  switch (type) {
+    case SET_USER_ADDRESS:
+      return {
+        ...state,
+        address: payload.address,
+      };
+    case SET_USER_LOADING:
+      return {
+        ...state,
+        loading: payload.loading,
+      };
+    case LOAD_USER_NEUMARKS:
+      return {
+        ...state,
+        neumarkAmmount: payload.neumarkAmmount,
+      };
     default:
       return state;
   }
