2982569a124a0d55337730ce3f86bfb9acdcc47d
Tomek Banach <tomek@banciur.org>
2017-08-03 18:11:13 +0200

    experiment

--- src/actions/commitActions.js
+++ src/actions/commitActions.js
@@ -1,6 +1,9 @@
 import sendETH from '../web3/sendETH';
+import { selectAddress } from '../reducers/icoParameters';
 
 // eslint-disable-next-line no-unused-vars,import/prefer-default-export
-export const commitETH = (ammount, userAddress) => async (dispatch) => {
-  sendETH(ammount, userAddress);
+export const commitETH = (ammount, userAddress) => async (dispatch, getState) => {
+  const state = getState();
+  const address = selectAddress(state.icoParameters);
+  sendETH(address, ammount, userAddress);
 };
--- src/web3/sendETH.js
+++ src/web3/sendETH.js
@@ -1,9 +1,21 @@
 import invariant from 'invariant';
+import { Crowdsale } from './contracts/index';
 
-export default async (ammount, userAddress) => {
+export default async (contractAddress, amount, userAddress) => {
   // @todo implement it
-  invariant(ammount, 'Specify ammount');
+  invariant(amount, 'Specify amount');
   invariant(userAddress, 'Specify userAddress');
 // eslint-disable-next-line no-console
-  console.log(`commiting ${ammount} ETH from ${userAddress}`);
+  console.log(`committing ${amount} ETH from ${userAddress}`);
+
+  const contract = Crowdsale(contractAddress);
+
+  contract.commit(
+    { value: amount * (10 ** 18), from: userAddress },
+    (err, result) => {
+      console.log('transaction');
+      console.log(err);
+      console.log(result);
+    }
+  );
 };
