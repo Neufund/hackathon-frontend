e5027cdef080b4b33e724c95b5cb16a424d606da
Tomek Banach <tomek@banciur.org>
2017-08-06 20:58:24 +0200

    Read user commitment from smartcontract

--- src/web3/loadUserNeumarks.js
+++
@@ -1,9 +0,0 @@
-import invariant from 'invariant';
-import Bluebird from 'bluebird';
-
-export default async (address) => {
-  // @todo implement it
-  invariant(address, 'Specify adddress!');
-  await Bluebird.delay(3000); // eslint-disable-line
-  return Promise.resolve(345098);
-};
